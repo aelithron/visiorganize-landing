0eeb83820e2daddd16ed3aa238a220a660a36e92
Aelithron <aelithron@gmail.com>
2025-06-03 21:09:30 -0600

    A bit of color fixing

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ import screenshot1 from "@/public/screenshots/1.webp";
 export default function Home() {
   return (
     <main className="flex flex-col items-center min-h-screen">
-      <div className="flex flex-col bg-gradient-to-br from-blue-300 to-purple-300 dark:from-blue-500 dark:to-purple-600 min-w-screen text-center p-8 md:px-20">
+      <div className="flex flex-col bg-gradient-to-b from-[#c9def4] to-[#f5ccd4] dark:from-[#0E0725] dark:to-[#5C03BC] min-w-screen text-center p-8 md:px-20">
         <div className="flex gap-4 text-center items-center justify-center mb-2">
           <Image src={logo} alt="Visiorganize Logo" width={70} height={70} />
           <h1 className={`font-semibold text-3xl`}>Visiorganize</h1>
@@ -16,7 +16,7 @@ export default function Home() {
         <p>The ultimate tool to organize software development work.</p>
         <div className="mt-4"><Link href={"https://visi.axionspire.net"} className="bg-blue-500 hover:text-sky-500 p-2 rounded-3xl text-lg"><FontAwesomeIcon icon={faRocket} /> Launch</Link></div>
       </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-2 md:grid-rows-1 bg-purple-300 dark:bg-violet-600 min-w-screen text-center p-8 md:px-20 place-content-center gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 grid-rows-2 md:grid-rows-1 bg-gradient-to-b from-[#f5ccd4] to-[#b8a4c9] dark:from-[#5C03BC] dark:to-[#1a2766] min-w-screen text-center p-8 md:px-20 place-content-center gap-4">
         <div>
           <h1 className="font-semibold text-2xl">What is Visiorganize?</h1>
           <p>It&apos;s a simple organization tool for software devs!</p>
